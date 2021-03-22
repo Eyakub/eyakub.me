0f3f725994a355b88825f398337924ea3250717e
eyakub <eyakubsorkar@gmail.com>
2021-03-23 05:29:00 +0600

    avator section done so far

--- portfolio_react_api/src/App.js
+++ portfolio_react_api/src/App.js
@@ -1,11 +1,20 @@
-
+import React from 'react';
+import About from './pages/About';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App p-3 container mx-auto md:container md:mx-auto sm:container sm:mx-auto">
+      <div className="grid grid-cols-1 justify-items-end">
+        <ul className="list-none inline">
+          <li>About</li>
+        </ul>
+      </div>
 
-    </div>
-  );
+      {/* information body */}
+      <div className="mt-5">
+        <About />
+      </div>
+    </div>);
 }
 
 export default App;
---
+++ portfolio_react_api/src/components/Avator/Details.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DetailSection = () => {
+    return (
+        <div>Details</div>
+    )
+}
+
+export default DetailSection;
