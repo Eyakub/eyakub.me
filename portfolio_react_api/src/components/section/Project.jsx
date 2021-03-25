31a43c07773168a65ae3aa6fc492bcb0c49f187b
eyakub <eyakubsorkar@gmail.com>
2021-03-25 06:13:11 +0600

    project section

--- portfolio_react_api/src/components/section/Project.jsx
+++ portfolio_react_api/src/components/section/Project.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tag from "../tags/TagBoard";
 
 const Project = () => {
   return (
@@ -6,6 +7,56 @@ const Project = () => {
       <h1 className="font-bold font-sans text-2xl uppercase text-blue-500">
         Projects
       </h1>
+      <div className="flex flex-wrap">
+        <article className="mt-2">
+          <h1 className="font-bold text-1xl mb-0 mt-1">
+            <a href="https://eyakub.me">Home Rent API</a>
+          </h1>
+          <p className="text-xs">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            dolorum voluptatem harum eaque magni labore iste distinctio, alias
+            dignissimos ut accusantium fugit incidunt cum atque rerum nostrum
+            non, voluptas velit!
+          </p>
+          <Tag />
+        </article>
+        <article className="mt-2">
+          <h1 className="font-bold text-1xl mb-0 mt-1">
+            <a href="https://eyakub.me">Home Rent API</a>
+          </h1>
+          <p className="text-xs">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            dolorum voluptatem harum eaque magni labore iste distinctio, alias
+            dignissimos ut accusantium fugit incidunt cum atque rerum nostrum
+            non, voluptas velit!
+          </p>
+          <Tag />
+        </article>
+        <article className="mt-2">
+          <h1 className="font-bold text-1xl mb-0 mt-1">
+            <a href="https://eyakub.me">Home Rent API</a>
+          </h1>
+          <p className="text-xs">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            dolorum voluptatem harum eaque magni labore iste distinctio, alias
+            dignissimos ut accusantium fugit incidunt cum atque rerum nostrum
+            non, voluptas velit!
+          </p>
+          <Tag />
+        </article>
+        <article className="mt-2">
+          <h1 className="font-bold text-1xl mb-0 mt-1">
+            <a href="https://eyakub.me">Home Rent API</a>
+          </h1>
+          <p className="text-xs">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            dolorum voluptatem harum eaque magni labore iste distinctio, alias
+            dignissimos ut accusantium fugit incidunt cum atque rerum nostrum
+            non, voluptas velit!
+          </p>
+          <Tag />
+        </article>
+      </div>
     </section>
   );
 };
